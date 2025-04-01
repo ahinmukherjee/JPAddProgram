60aab072e807a2471e6b1f2389ad7c716dd847dc
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-04-01 12:09:42 +0530

    files added

---
+++ Components/FramePanelBackground.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class FramePanelBackground {
+    public static void main(String args[]){
+        FrameTest ft= new FrameTest();
+        ft.setVisible(true);
+        ft.setDefaultCloseOperation(3);   	
+    } 
+}   
+    
+class FrameTest extends JFrame{
+    public FrameTest(){
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension scrsize = kit.getScreenSize();
+        int w = scrsize.width;
+        int h = scrsize.height;
+        setSize(w/2, h/2);
+        setLocation(w/4, h/4);
+        setTitle("Welcome in Frame");
+        setResizable(false);
+        FramePanels fp = new FramePanels();
+        add(fp);
+    }
+} 
+    
+class FramePanels extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Font f = new Font("serif",Font.BOLD+Font.ITALIC,20);
+        setFont(f);
+        g.drawString("I Love Java",100,200);
+    }
+}		
+
